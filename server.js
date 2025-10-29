5c543f7eb8ac4121ec52bbfa4c34924383e61829
vishaligt <vishal.igtechso@gmail.com>
2025-10-29 12:20:27 +0530

    Initial backend docker deploy

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-
+const redis = require("redis");
 const app = express();
 
 
@@ -9,6 +9,8 @@ const app = express();
 
 const PORT = 5000;
 const MONGO_URI = "mongodb+srv://vishal:12345@cluster0.hpcdmz5.mongodb.net/?appName=Cluster0";
+
+const REDIS_URL = process.env.REDIS_URL || "redis://redis:6379";
 
 //const MONGO_URI = "mongodb://localhost:27017/mydb" ;
 app.use(cors());
@@ -19,6 +21,14 @@ mongoose
   .connect(MONGO_URI)
   .then(() => console.log("✅ Connected to MongoDB"))
   .catch((err) => console.error("❌ MongoDB connection error:", err));
+
+
+  //redis 
+
+  const redisClient = redis.createClient({url: REDIS_URL});
+  redisClient.connect()
+  .then(()=>console.log("redis conntected"))
+  .catch((err)=>console.error("redis error" , err));
 
 // Schema
 const formSchema = new mongoose.Schema({
@@ -38,6 +48,11 @@ app.post("/api/form", async (req, res) => {
     const { name, email, message } = req.body;
     const form = new Form({ name, email, message });
     await form.save();
+      // clear cache after new insert
+   const v = await redisClient.del("forms_cache");
+   if(v){
+    console.log("safcdsaddsadsaa");
+   }
     res.status(201).json({ success: true, data: form });
   } catch (err) {
     res.status(500).json({ success: false, error: err.message });
@@ -45,8 +60,26 @@ app.post("/api/form", async (req, res) => {
 });
 
 app.get("/api/forms", async (req, res) => {
+
+
+  try{
+    const cacheData = await redisClient.get("forms_cache");
+    if(cacheData){
+      console.log("catch data");
+      return res.json(JSON.parse(cacheData));
+    }
+  
   const forms = await Form.find().sort({ createdAt: -1 });
+  // set cache for 60 seconds
+    await redisClient.setEx("forms_cache", 60, JSON.stringify(forms));
+
   res.json(forms);
+  }catch(err){
+    res.status(500).json({error : err.message});
+  }
 });
 
-app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
+app.listen(PORT, "0.0.0.0", () =>
+  console.log(`✅ Server running on port ${PORT}`)
+);
+
